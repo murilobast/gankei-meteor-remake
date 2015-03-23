d8d6704f1235046f3f03f732cd5e74eb1937531b
murilobast <mbast@live.com>
2015-03-22 22:41:20 -0300

    updated login form and added user options modal

--- client/header/header.js
+++ client/header/header.js
@@ -4,7 +4,7 @@ Template.header.helpers({
 	}
 })
 Template.header.events({
-	'click .header__menu': function(e, t){
+	'click .header__menu': function(){
 		var side = $('.content__side');
 		if (side.css('margin-left') == '0px'){
 			side.css('margin-left', '-230px');
@@ -12,7 +12,33 @@ Template.header.events({
 			side.css('margin-left', '0px');
 		}
 	},
-	'click .header__profile__settings': function(){
+	'click .header__modal__links__logout': function(){
 		Meteor.logout();
+	},
+	'click .header__profile__avatar': function(){
+		var modal = $('.header__modal');
+		if (modal.css('height') == '90px'){
+			modal.css('height', '0px');
+		}else{
+			modal.css('height', '90px');
+		}
+	},
+	'click .header__profile__login': function(){
+		var modal = $('.header__modal');
+		if (modal.css('height') == '144px'){
+			modal.css('height', '0px');
+		}else{
+			modal.css('height', '144px');
+		}
+	}
+})
+Template.body.events({
+	'click a': function(){
+		var modal = $('.header__modal');
+		modal.css('height', '0px');
+	},
+	'click button': function(){
+		var modal = $('.header__modal');
+		modal.css('height', '0px');
 	}
 })
